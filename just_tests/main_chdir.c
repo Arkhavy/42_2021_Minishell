74b322dabb2c4bc4fb04f8a65b28db5722cb23d8
Loic Johnson <ljohnson@z4r10p5.42lyon.fr>
2022-03-16 13:12:59 +0100

    updated chdir tests

--- just_tests/main_chdir.c
+++ just_tests/main_chdir.c
@@ -6,7 +6,7 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 13:23:56 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/03/12 13:24:58 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2022/03/16 10:17:06 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 
 int	main(void)
 {
-	dprintf(1, "%d\n", chdir(NULL));
+	int	fd;
+
+	fd = open("outfile.txt", O_CREAT | O_RDWR | O_TRUNC, 0644);
+	dprintf(1, "getcwd before: %s\n", getcwd(NULL, 0));
+	chdir("../..");
+	dprintf(fd, "getcwd after: %s\n", getcwd(NULL, 0));
+	close (fd);
 	return (0);
 }
