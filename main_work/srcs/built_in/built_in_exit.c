ff05b2c5a8e961af230cc8319cbd6c83a624a545
Loic Johnson <ljohnson@z4r10p5.42lyon.fr>
2022-04-14 14:23:57 +0200

    removed fds from built-ins, will be dealt separately in exec

--- main_work/srcs/built_in/built_in_exit.c
+++ main_work/srcs/built_in/built_in_exit.c
@@ -6,7 +6,7 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 08:23:08 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/04/04 10:27:31 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2022/04/14 14:21:33 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ static int	mini_check_raw_arg(char *raw_arg)
 	return (0);
 }
 
-int	mini_exit_built_in(t_master *master, char *raw_arg, int fd_out)
+int	mini_exit_built_in(t_master *master, char *raw_arg)
 {
 	t_env	*env_var;
 	int		check;
@@ -50,7 +50,7 @@ int	mini_exit_built_in(t_master *master, char *raw_arg, int fd_out)
 	env_var = mini_get_env_var(master->envdata, "SHLVL");
 	if (!env_var)
 		env_var = mini_set_env_var(master->envdata, "SHLVL", "1");
-	ft_putendl_fd("exit", fd_out);
+	ft_putendl_fd("exit", 1);
 	if (!check)
 		g_mini_errno = ft_atoi(raw_arg);
 	else if (check == 1)
