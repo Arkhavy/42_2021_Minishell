2ea8acd929508b8b644fbc435d7348f7d29f4b1a
Pierre <pierre.lvrgn@gmail.com>
2022-04-16 13:43:23 +0200

    rm secure strlen

--- main_work/libft/srcs/counters/ft_strlen.c
+++ main_work/libft/srcs/counters/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: plavergn <plavergn@student.42lyon.fr >     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 12:04:00 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/04/16 09:52:05 by plavergn         ###   ########.fr       */
+/*   Updated: 2022/04/16 13:42:44 by plavergn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ size_t	ft_strlen(const char *str)
 	size_t	a;
 
 	a = 0;
-	if (!str)
-		return (0);
 	while (str[a])
 		a++;
 	return (a);
