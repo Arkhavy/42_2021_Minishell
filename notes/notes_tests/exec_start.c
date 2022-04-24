2fdd1beccd1facc8ad49204361fad978cf95e65b
Loic Johnson <ljohnson@z4r10p5.42lyon.fr>
2022-04-24 11:04:35 +0200

    added another version of exec in start notes

--- notes/notes_tests/exec_start.c
+++ notes/notes_tests/exec_start.c
@@ -6,7 +6,7 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/16 08:02:37 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/04/19 13:50:24 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2022/04/23 08:39:14 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -150,6 +150,111 @@ int	mini_execution_loop(t_master *master)
 	{
 		waitpid(-1, 0, 0);
 		a--;
+	}
+	return (0);
+}
+*/
+
+/*
+int	mini_redirection(void)
+{
+	char	buffer;
+	int		index;
+	// int		fd_in;
+	// int		fd_out;
+
+	index = 1;
+	while (index)
+	{
+		index = read(STDIN_FILENO, &buffer, 1);
+		if (index < 0)
+			return (mini_error_print(E_READ, DFI, DLI, DFU));
+		if (index)
+		{
+			if (write(STDOUT_FILENO, &buffer, 1) == -1)
+				return (mini_error_print(E_WRITE, DFI, DLI, DFU));
+		}
+	}
+	// if (close (fd_in) == -1)
+	// 	return (mini_error_print(E_CLOSE, DFI, DLI, DFU));
+	// if (close (fd_out) == -1)
+	// 	return (mini_error_print(E_CLOSE, DFI, DLI, DFU));
+	return (0);
+}
+
+int	mini_exec_loop(t_master *master)
+{
+	size_t	a;
+	t_cmd	*cmd;
+	pid_t	*pid;
+	int		pipe_fd[2];
+	int		fd_link;
+
+	a = 0;
+	master->execdata->lst = master->execdata->start;
+	pid = ft_calloc(master->execdata->lst_size, sizeof(pid_t));
+	if (!pid)
+		return (mini_error_print(E_MALLOC, DFI, DLI, DFU));
+	fd_link = dup(STDIN_FILENO);
+	if (fd_link == -1)
+		return (mini_error_print(E_DUP, DFI, DLI, DFU));
+	while (master->execdata->lst)
+	{
+		cmd = master->execdata->lst->content;
+		if (!master->execdata->lst->next && cmd->token_id != IDT_REDIR)
+		{
+			if (mini_reset_fdstruct(master->fdstruct))
+				return (1);
+		}
+		else
+		{
+			if (pipe(pipe_fd) == -1)
+				return (mini_error_print(E_PIPE, DFI, DLI, DFU));
+		}
+		pid[a] = fork();
+		if (pid[a] == -1)
+			return (mini_error_print(E_FORK, DFI, DLI, DFU));
+		else if (!pid[a])
+		{
+			if (master->execdata->lst->next)
+			{
+				if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
+					return (mini_error_print(E_DUP2, DFI, DLI, DFU));
+			}
+			if (dup2(fd_link, STDIN_FILENO) == -1)
+				return (mini_error_print(E_DUP2, DFI, DLI, DFU));
+			if (cmd->token_id == IDT_CMD)
+				exit (mini_execve(master->envdata, cmd));
+			else if (cmd->token_id == IDT_BTIN)
+				exit (mini_built_in_hub(master, cmd));
+			else if (cmd->token_id == IDT_REDIR)
+				exit (mini_redirection());
+			if (master->execdata->lst->next)
+			{
+				if (close (pipe_fd[0]) == -1)
+					return (mini_error_print(E_CLOSE, DFI, DLI, DFU));
+			}
+			else
+			{
+				if (dup2(master->fdstruct->fd_in, STDIN_FILENO) == -1)
+					return (mini_error_print(E_DUP2, DFI, DLI, DFU));
+			}
+			if (close (fd_link) == -1)
+				return (mini_error_print(E_CLOSE, DFI, DLI, DFU));
+			exit(0);
+		}
+		if (close (fd_link) == -1)
+			return (mini_error_print(E_CLOSE, DFI, DLI, DFU));
+		fd_link = dup(pipe_fd[0]);
+		if (fd_link == -1)
+			return (mini_error_print(E_DUP, DFI, DLI, DFU));
+		if (close (pipe_fd[0]) == -1)
+			return (mini_error_print(E_CLOSE, DFI, DLI, DFU));
+		if (close (pipe_fd[1]) == -1)
+			return (mini_error_print(E_CLOSE, DFI, DLI, DFU));
+		if (mini_reset_fdstruct(master->fdstruct))
+			return (1);
+		master->execdata->lst = master->execdata->lst->next;
 	}
 	return (0);
 }
