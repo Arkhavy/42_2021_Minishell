419f95a4def26fb1e16c9c07e273a96aaa2b07fa
Loic Johnson <ljohnson@z4r10p5.42lyon.fr>
2022-05-07 15:09:58 +0200

    added some error messages to work on, slowly adding things

--- final_minishell/srcs/init/init_master.c
+++ final_minishell/srcs/init/init_master.c
@@ -6,18 +6,21 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 12:58:55 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/05/07 13:15:22 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2022/05/07 13:23:13 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
-
 int	mini_init_master(t_master *master, char **env)
 {
 	master->envdata = ft_calloc(1, sizeof(t_envdata));
 	master->fdstruct = ft_calloc(1, sizeof(t_fdstruct));
 	master->execdata = ft_calloc(1, sizeof(t_execdata));
 	if (!master->envdata || !master->fdstruct || !master->execdata)
-		return (mini_error(E_MALLOC, NULL, N_MALLOC, 1));
-	if ()
+		return (1);
+	if (mini_init_envdata(master->envdata, env))
+		return (1);
+	if (mini_init_fdstruct(master->fdstruct))
+		return (1);
+	return (0);
 }
