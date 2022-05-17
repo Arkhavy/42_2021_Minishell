99d8a2f6ddeb8ac4e4925638f20dd49eb6afd651
Pierre <pierre.lvrgn@gmail.com>
2022-05-17 13:42:20 +0200

    opti function split_echo

--- main_work/srcs/parsing/parsing_test.c
+++ main_work/srcs/parsing/parsing_test.c
@@ -6,7 +6,7 @@
 /*   By: plavergn <plavergn@student.42lyon.fr >     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 09:42:47 by plavergn          #+#    #+#             */
-/*   Updated: 2022/05/17 12:54:03 by plavergn         ###   ########.fr       */
+/*   Updated: 2022/05/17 13:38:06 by plavergn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,97 +91,6 @@ int	check_token_id(char *split, int len_cmd)
 	return (token_id);
 }
 
-// int	count_d_q_word(char *str)
-// {
-// 	int	i;
-// 	int	d_q;
-
-// 	i = 0;
-// 	d_q = 0;
-// 	while (str[i])
-// 	{
-// 		if (str[i] == '"')
-// 		{
-// 			i++;
-// 			while (str[i] && str[i] != '"')
-// 				i++;
-// 			d_q++;
-// 		}
-// 		else if (str[i] == ' ' && d_q == 0)
-// 		{
-// 			while (str[i] && str[i] == ' ')
-// 				i++;
-// 			d_q++;
-// 		}
-// 		else
-// 			i++;
-// 	}
-// 	return (d_q);
-// }
-
-// char	**double_quote(char *str)
-// {
-// 	int		i;
-// 	int		j;
-// 	int		a;
-// 	char	**split;
-// 	char	*tmp;
-
-// 	i = 0;
-// 	j = 0;
-// 	a = 0;
-// 	split = malloc(sizeof(char *) * (count_d_q_word(str) + 2));
-// 	while (str[i])
-// 	{
-// 		if (str[i] == '"')
-// 		{
-// 			i++;
-// 			a = i;
-// 			while (str[i] && str[i] != '"')
-// 				i++;
-// 			tmp = ft_substr(str, a, i - a);
-// 			if (!split[j])
-// 				split[j] = ft_strdup(tmp);
-// 			else
-// 				split[j] = ft_strjoin(split[j], tmp);
-// 			// printf("-->%s<--\n", split[j]);
-// 			a = i + 1;
-// 		}
-// 		else if (str[i] == ' ' && j == 0)
-// 		{
-// 			split[j] = ft_substr(str, a, i - a);
-// 			// printf("-->%s<--\n", split[j]);
-// 			j++;
-// 			split[j] = NULL;
-// 			while (str[i] == ' ')
-// 				i++;
-// 			a = i;
-// 		}
-// 		else if (str[i + 1] == '\0')
-// 		{
-// 			tmp = ft_substr(str, a, i + 1 - a);
-// 			if (!split[j])
-// 				split[j] = ft_strdup(tmp);
-// 			else
-// 				split[j] = ft_strjoin(split[j], tmp);
-// 			// printf("-->%s<--\n", split[j]);
-// 			i++;
-// 		}
-// 		else
-// 			i++;
-// 		// printf("oui: %d\n", i);
-// 	}
-// 	j++;
-// 	// printf("ok\n");
-// 	split[j] = NULL;
-// 	while (split[j])
-// 	{
-// 		// printf("%d\n", j);
-// 		j++;
-// 	}
-// 	return (split);
-// }
-
 int	count_echo(char *str)
 {
 	int	i;
@@ -228,19 +137,13 @@ int	count_echo(char *str)
 // int	count_cd(char *str)
 // {}
 
-int	count_arg_echo(char *str, int len_cmd, int n)
+int	count_arg_echo(char *str)
 {
 	int	i;
 	int	len;
 
 	len = 0;
-	i = len_cmd;
-	if (n == 0)
-		while (str[i] && (str[i] == ' ' || str[i] == '-' || str[i] == 'n'))
-			i++;
-	else
-		while (str[i] && str[i] == ' ')
-			i++;
+	i = 0;
 	while (str[i])
 	{
 		if (str[i] != '"')
@@ -250,57 +153,53 @@ int	count_arg_echo(char *str, int len_cmd, int n)
 	return (len);
 }
 
-char	**split_echo(char *str, char *cmd, int len)
+char	*put_without_d_q(char *str, char *split, int len)
 {
-	char	**split;
-	int		i;
-	int		n;
-	int		j;
-	int		a;
+	int	i;
+	int	a;
 
-	n = -1;
-	i = len;
 	a = 0;
-	j = 1;
-	split = NULL;
-	while (str[i] == ' ')
-		i++;
-	if (str[i] == '-')
+	i = 0;
+	split = malloc(sizeof(char) * (len + 1));
+	while (str[i])
 	{
-		i++;
-		while (str[i] && str[i] != ' ')
+		if (str[i] != '"')
 		{
-			if (str[i] != 'n')
-				n = 1;
-			else
-				n = 0;
-			i++;
+			split[a] = str[i];
+			a++;
 		}
+		i++;
 	}
-	if (n == 0)
+	split[a] = '\0';
+	return (split);
+}
+
+char	**split_echo(char *str, char *cmd, int len, char **split)
+{
+	int		i;
+	int		j;
+
+	j = 1;
+	i = ft_int_strchr(str, '-') + 1;
+	while (str[i] && str[i] != ' ' && str[i] == 'n')
+		i++;
+	if (str[i] == ' ')
 	{
 		split = malloc(sizeof(char *) * (4));
 		split[1] = ft_strdup("-n");
 		j = 2;
 	}
 	else
-		split = malloc(sizeof(char *) * (3));
-	split[0] = ft_strdup(cmd);
-	len = count_arg_echo(str, len, n);
-	split[j] = ft_calloc(len + 2, sizeof(char));
-	// split[j] = ft_substr(str, i, ft_strlen(str));
-	while (str[i])
 	{
-		if (str[i] != '"')
-		{
-			split[j][a] = str[i];
-			a++;
-		}
-		i++;
+		i = len;
+		split = malloc(sizeof(char *) * (3));
 	}
-	split[j][a] = '\0';
+	while (str[i] && str[i] == ' ')
+		i++;
+	split[0] = ft_strdup(cmd);
+	len = count_arg_echo(&str[i]);
+	split[j] = put_without_d_q(&str[i], split[j], len + 1);
 	split[j + 1] = NULL;
-	// printf("%s\n", split[1]);
 	return (split);
 }
 
@@ -359,11 +258,11 @@ char	**split_echo(char *str, char *cmd, int len)
 // }
 
 
-char	**check_split(char *cmd, char *str, int len)
+char	**check_split(char *cmd, char *str, int len, char **split)
 {
 	if (!ft_strncmp(cmd, "echo",
 			ft_get_highest(len, ft_strlen("echo"))))
-		return (split_echo(str, cmd, len));
+		return (split_echo(str, cmd, len, split));
 	// if (!ft_strncmp(cmd, "pwd",
 	// 		ft_get_highest(len, ft_strlen("pwd"))))
 	// 	return (split_pwd(str, cmd, len));
@@ -400,7 +299,7 @@ int	init_cmd(char *str, char *dest, t_master *master, int d_q)
 	tmp = ft_split(dest, ' ');
 	cmd->len_cmd = ft_strlen(tmp[0]);
 	cmd->token_id = check_token_id(tmp[0], cmd->len_cmd);
-	cmd->split = check_split(tmp[0], dest, cmd->len_cmd);
+	cmd->split = check_split(tmp[0], dest, cmd->len_cmd, cmd->split);
 	ft_free_split(tmp);
 	ft_lstadd_back(&master->execdata->lst, ft_lstnew(cmd));
 	master->execdata->lst_size++;
