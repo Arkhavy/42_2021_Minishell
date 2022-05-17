302e8b445d2920ea613ddce16a889e9fbe971353
Pierre <pierre.lvrgn@gmail.com>
2022-05-17 12:56:26 +0200

    echo fix

--- main_work/srcs/parsing/parsing_test.c
+++ main_work/srcs/parsing/parsing_test.c
@@ -6,7 +6,7 @@
 /*   By: plavergn <plavergn@student.42lyon.fr >     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 09:42:47 by plavergn          #+#    #+#             */
-/*   Updated: 2022/05/17 08:53:20 by plavergn         ###   ########.fr       */
+/*   Updated: 2022/05/17 12:54:03 by plavergn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,13 +91,104 @@ int	check_token_id(char *split, int len_cmd)
 	return (token_id);
 }
 
-int	count_d_q_word(char *str)
+// int	count_d_q_word(char *str)
+// {
+// 	int	i;
+// 	int	d_q;
+
+// 	i = 0;
+// 	d_q = 0;
+// 	while (str[i])
+// 	{
+// 		if (str[i] == '"')
+// 		{
+// 			i++;
+// 			while (str[i] && str[i] != '"')
+// 				i++;
+// 			d_q++;
+// 		}
+// 		else if (str[i] == ' ' && d_q == 0)
+// 		{
+// 			while (str[i] && str[i] == ' ')
+// 				i++;
+// 			d_q++;
+// 		}
+// 		else
+// 			i++;
+// 	}
+// 	return (d_q);
+// }
+
+// char	**double_quote(char *str)
+// {
+// 	int		i;
+// 	int		j;
+// 	int		a;
+// 	char	**split;
+// 	char	*tmp;
+
+// 	i = 0;
+// 	j = 0;
+// 	a = 0;
+// 	split = malloc(sizeof(char *) * (count_d_q_word(str) + 2));
+// 	while (str[i])
+// 	{
+// 		if (str[i] == '"')
+// 		{
+// 			i++;
+// 			a = i;
+// 			while (str[i] && str[i] != '"')
+// 				i++;
+// 			tmp = ft_substr(str, a, i - a);
+// 			if (!split[j])
+// 				split[j] = ft_strdup(tmp);
+// 			else
+// 				split[j] = ft_strjoin(split[j], tmp);
+// 			// printf("-->%s<--\n", split[j]);
+// 			a = i + 1;
+// 		}
+// 		else if (str[i] == ' ' && j == 0)
+// 		{
+// 			split[j] = ft_substr(str, a, i - a);
+// 			// printf("-->%s<--\n", split[j]);
+// 			j++;
+// 			split[j] = NULL;
+// 			while (str[i] == ' ')
+// 				i++;
+// 			a = i;
+// 		}
+// 		else if (str[i + 1] == '\0')
+// 		{
+// 			tmp = ft_substr(str, a, i + 1 - a);
+// 			if (!split[j])
+// 				split[j] = ft_strdup(tmp);
+// 			else
+// 				split[j] = ft_strjoin(split[j], tmp);
+// 			// printf("-->%s<--\n", split[j]);
+// 			i++;
+// 		}
+// 		else
+// 			i++;
+// 		// printf("oui: %d\n", i);
+// 	}
+// 	j++;
+// 	// printf("ok\n");
+// 	split[j] = NULL;
+// 	while (split[j])
+// 	{
+// 		// printf("%d\n", j);
+// 		j++;
+// 	}
+// 	return (split);
+// }
+
+int	count_echo(char *str)
 {
 	int	i;
-	int	d_q;
+	int	a;
 
 	i = 0;
-	d_q = 0;
+	a = 0;
 	while (str[i])
 	{
 		if (str[i] == '"')
@@ -105,74 +196,212 @@ int	count_d_q_word(char *str)
 			i++;
 			while (str[i] && str[i] != '"')
 				i++;
-			d_q++;
+			a++;
 		}
-		else if (str[i] == ' ')
+		else if (str[i] == ' ' && a == 0)
 		{
 			while (str[i] && str[i] == ' ')
 				i++;
-			d_q++;
+			a++;
 		}
 		else
 			i++;
 	}
-	return (d_q);
+	return (a);
 }
 
-char	**double_quote(char *str)
-{
-	int		i;
-	int		j;
-	int		a;
-	char	**split;
+// int	count_pwd(char *str)
+// {}
 
-	i = 0;
-	j = 0;
-	a = 0;
-	split = malloc(sizeof(char *) * (count_d_q_word(str) + 1));
+// int	count_env(char *str)
+// {}
+
+// int	count_exit(char *str)
+// {}
+
+// int	count_export(char *str)
+// {}
+
+// int	count_unset(char *str)
+// {}
+
+// int	count_cd(char *str)
+// {}
+
+int	count_arg_echo(char *str, int len_cmd, int n)
+{
+	int	i;
+	int	len;
+
+	len = 0;
+	i = len_cmd;
+	if (n == 0)
+		while (str[i] && (str[i] == ' ' || str[i] == '-' || str[i] == 'n'))
+			i++;
+	else
+		while (str[i] && str[i] == ' ')
+			i++;
 	while (str[i])
 	{
-		if (str[i] == '"')
-		{
-			i++;
-			split[j] = ft_substr(str, i, ft_int_strchr(&str[i], '"'));
-			j++;
-			while (str[i] && str[i] != '"')
-				i++;
-		}
-		else if (str[i] == ' ')
-		{
-			while (str[i] == ' ')
-				i++;
-			split[j] = ft_substr(str, i, ft_int_strchr(&str[i], ' '));
-			j++;
-		}
-		else if (str[i + 1] == '\0')
-		{
-			split[j] = ft_substr(str, i, ft_int_strchr(&str[i], '\0'));
-			j++;
-		}
-		else
-			i++;
+		if (str[i] != '"')
+			len++;
+		i++;
 	}
-	split[j] = NULL;
+	return (len);
+}
+
+char	**split_echo(char *str, char *cmd, int len)
+{
+	char	**split;
+	int		i;
+	int		n;
+	int		j;
+	int		a;
+
+	n = -1;
+	i = len;
+	a = 0;
+	j = 1;
+	split = NULL;
+	while (str[i] == ' ')
+		i++;
+	if (str[i] == '-')
+	{
+		i++;
+		while (str[i] && str[i] != ' ')
+		{
+			if (str[i] != 'n')
+				n = 1;
+			else
+				n = 0;
+			i++;
+		}
+	}
+	if (n == 0)
+	{
+		split = malloc(sizeof(char *) * (4));
+		split[1] = ft_strdup("-n");
+		j = 2;
+	}
+	else
+		split = malloc(sizeof(char *) * (3));
+	split[0] = ft_strdup(cmd);
+	len = count_arg_echo(str, len, n);
+	split[j] = ft_calloc(len + 2, sizeof(char));
+	// split[j] = ft_substr(str, i, ft_strlen(str));
+	while (str[i])
+	{
+		if (str[i] != '"')
+		{
+			split[j][a] = str[i];
+			a++;
+		}
+		i++;
+	}
+	split[j][a] = '\0';
+	split[j + 1] = NULL;
+	// printf("%s\n", split[1]);
 	return (split);
+}
+
+// char	**split_pwd(char *str, char *cmd, int len)
+// {
+// 	char	**split;
+// 	int		i;
+
+// 	i = 0;
+// 	split[0] = cmd;
+// }
+
+// char	**split_env(char *str, char *cmd, int len)
+// {
+// 	char	**split;
+// 	int		i;
+
+// 	i = 0;
+// 	split[0] = cmd;
+// }
+
+// char	**split_exit(char *str, char *cmd, int len)
+// {
+// 	char	**split;
+// 	int		i;
+
+// 	i = 0;
+// 	split[0] = cmd;
+// }
+
+// char	**split_export(char *str, char *cmd, int len)
+// {
+// 	char	**split;
+// 	int		i;
+
+// 	i = 0;
+// 	split[0] = cmd;
+// }
+
+// char	**split_unset(char *str, char *cmd, int len)
+// {
+// 	char	**split;
+// 	int		i;
+
+// 	i = 0;
+// 	split[0] = cmd;
+// }
+
+// char	**split_cd(char *str, char *cmd, int len)
+// {
+// 	char	**split;
+// 	int		i;
+
+// 	i = 0;
+// 	split[0] = cmd;
+// }
+
+
+char	**check_split(char *cmd, char *str, int len)
+{
+	if (!ft_strncmp(cmd, "echo",
+			ft_get_highest(len, ft_strlen("echo"))))
+		return (split_echo(str, cmd, len));
+	// if (!ft_strncmp(cmd, "pwd",
+	// 		ft_get_highest(len, ft_strlen("pwd"))))
+	// 	return (split_pwd(str, cmd, len));
+	// if (!ft_strncmp(cmd, "env",
+	// 		ft_get_highest(len, ft_strlen("env"))))
+	// 	return (split_env(str, cmd, len));
+	// if (!ft_strncmp(cmd, "exit",
+	// 		ft_get_highest(len, ft_strlen("exit"))))
+	// 	return (split_exit(str, cmd, len));
+	// if (!ft_strncmp(cmd, "export",
+	// 		ft_get_highest(len, ft_strlen("export"))))
+	// 	return (split_export(str, cmd, len));
+	// if (!ft_strncmp(cmd, "unset",
+	// 		ft_get_highest(len, ft_strlen("unset"))))
+	// 	return (split_unset(str, cmd, len));
+	// if (!ft_strncmp(cmd, "cd",
+	// 		ft_get_highest(len, ft_strlen("cd"))))
+	// 	return (split_cd(str, cmd, len));
+	return (NULL);
 }
 
 int	init_cmd(char *str, char *dest, t_master *master, int d_q)
 {
 	t_cmd	*cmd;
+	char	**tmp;
 
 	cmd = ft_calloc(1, sizeof(t_cmd));
 	if (!cmd)
 		return (mini_error(ENOMEM));
 	cmd->raw = ft_strdup(str);
-	if (d_q == 1)
-		cmd->split = double_quote(dest);
-	else
-		cmd->split = ft_split(dest, ' '); //fix
-	cmd->len_cmd = ft_strlen(cmd->split[0]);
-	cmd->token_id = check_token_id(cmd->split[0], cmd->len_cmd);
+	(void) d_q;
+	// if (d_q == 1 || d_q == 0)
+	// 	cmd->split = double_quote(dest);
+	tmp = ft_split(dest, ' ');
+	cmd->len_cmd = ft_strlen(tmp[0]);
+	cmd->token_id = check_token_id(tmp[0], cmd->len_cmd);
+	cmd->split = check_split(tmp[0], dest, cmd->len_cmd);
+	ft_free_split(tmp);
 	ft_lstadd_back(&master->execdata->lst, ft_lstnew(cmd));
 	master->execdata->lst_size++;
 	return (0);
