04366688bed6519402546ede4493d688076e9251
Loic Johnson <ljohnson@z4r10p5.42lyon.fr>
2022-05-17 08:26:01 +0200

    added loops for built-ins

--- main_work/srcs/execution/exec_main.c
+++ main_work/srcs/execution/exec_main.c
@@ -6,7 +6,7 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 11:04:10 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/05/16 09:27:50 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2022/05/17 08:24:25 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,26 +37,21 @@ int	mini_execve(t_envdata *envdata, t_cmd *cmd)
 int	mini_built_in_hub(t_master *master, t_cmd *cmd)
 {
 	if (!ft_strncmp(cmd->split[0], "echo",
-			ft_get_highest(cmd->len_cmd, ft_strlen("echo"))))
-		exit (mini_echo_built_in(cmd->split));
-	if (!ft_strncmp(cmd->split[0], "pwd",
-			ft_get_highest(cmd->len_cmd, ft_strlen("pwd"))))
-		exit (mini_pwd_built_in());
-	if (!ft_strncmp(cmd->split[0], "env",
-			ft_get_highest(cmd->len_cmd, ft_strlen("env"))))
-		exit (mini_env_built_in(master->envdata));
-	if (!ft_strncmp(cmd->split[0], "exit",
-			ft_get_highest(cmd->len_cmd, ft_strlen("exit"))))
-		exit (0);
-	if (!ft_strncmp(cmd->split[0], "export",
-			ft_get_highest(cmd->len_cmd, ft_strlen("export"))))
-		exit (mini_export_built_in(master->envdata, cmd->split[1]));
-	if (!ft_strncmp(cmd->split[0], "unset",
-			ft_get_highest(cmd->len_cmd, ft_strlen("unset"))))
-		exit (mini_unset_built_in(master->envdata, cmd->split[1]));
-	if (!ft_strncmp(cmd->split[0], "cd",
+			ft_get_highest(cmd->len_cmd, ft_strlen("echo")))
+		|| !ft_strncmp(cmd->split[0], "pwd",
+			ft_get_highest(cmd->len_cmd, ft_strlen("pwd")))
+		|| !ft_strncmp(cmd->split[0], "env",
+			ft_get_highest(cmd->len_cmd, ft_strlen("env")))
+		|| !ft_strncmp(cmd->split[0], "exit",
+			ft_get_highest(cmd->len_cmd, ft_strlen("exit")))
+		|| !ft_strncmp(cmd->split[0], "cd",
 			ft_get_highest(cmd->len_cmd, ft_strlen("cd"))))
-		exit (mini_cd_built_in(master->envdata, cmd->split[1]));
+		return (mini_built_in_part_one(master, cmd));
+	else if (!ft_strncmp(cmd->split[0], "export",
+			ft_get_highest(cmd->len_cmd, ft_strlen("export")))
+		|| !ft_strncmp(cmd->split[0], "unset",
+			ft_get_highest(cmd->len_cmd, ft_strlen("unset"))))
+		return (mini_built_in_part_two(master, cmd));
 	return (-1);
 }
 
--- main_work/srcs/managers/manage_execution.c
+++ main_work/srcs/managers/manage_execution.c
@@ -6,7 +6,7 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 11:07:18 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/05/08 11:07:34 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2022/05/17 08:20:04 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,52 @@ int	mini_exec_hub(t_master *master, t_cmd *cmd)
 	else if (cmd->token_id == IDT_REDIR)
 		return (mini_redirection_hub(cmd));
 	return (0);
+}
+
+int	mini_built_in_part_one(t_master *master, t_cmd *cmd)
+{
+	if (!ft_strncmp(cmd->split[0], "echo",
+			ft_get_highest(cmd->len_cmd, ft_strlen("echo"))))
+		exit (mini_echo_built_in(cmd->split));
+	if (!ft_strncmp(cmd->split[0], "pwd",
+			ft_get_highest(cmd->len_cmd, ft_strlen("pwd"))))
+		exit (mini_pwd_built_in());
+	if (!ft_strncmp(cmd->split[0], "env",
+			ft_get_highest(cmd->len_cmd, ft_strlen("env"))))
+		exit (mini_env_built_in(master->envdata));
+	if (!ft_strncmp(cmd->split[0], "exit",
+			ft_get_highest(cmd->len_cmd, ft_strlen("exit"))))
+		exit (0);
+	if (!ft_strncmp(cmd->split[0], "cd",
+			ft_get_highest(cmd->len_cmd, ft_strlen("cd"))))
+		exit (mini_cd_built_in(master->envdata, cmd->split[1]));
+	return (-1);
+}
+
+int	mini_built_in_part_two(t_master *master, t_cmd *cmd)
+{
+	int	a;
+
+	a = 1;
+	if (!ft_strncmp(cmd->split[0], "export",
+			ft_get_highest(cmd->len_cmd, ft_strlen("export"))))
+	{
+		while (cmd->split[a])
+		{
+			mini_export_built_in(master->envdata, cmd->split[a]);
+			a++;
+		}
+		exit (0);
+	}
+	if (!ft_strncmp(cmd->split[0], "unset",
+			ft_get_highest(cmd->len_cmd, ft_strlen("unset"))))
+	{
+		while (cmd->split[a])
+		{
+			mini_unset_built_in(master->envdata, cmd->split[a]);
+			a++;
+		}
+		exit (0);
+	}
+	return (-1);
 }
