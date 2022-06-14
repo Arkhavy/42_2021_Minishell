3af64973dbd7a269a74d5b35b57351d1458aeeb0
Pierre <pierre.lvrgn@gmail.com>
2022-06-14 14:41:14 +0200

    export avec arg

--- main_work/srcs/parsing/parsing_export.c
+++ main_work/srcs/parsing/parsing_export.c
@@ -6,17 +6,17 @@
 /*   By: plavergn <plavergn@student.42lyon.fr >     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 10:22:56 by plavergn          #+#    #+#             */
-/*   Updated: 2022/06/14 11:49:52 by plavergn         ###   ########.fr       */
+/*   Updated: 2022/06/14 14:40:52 by plavergn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incs/minishell.h"
 
-char	*export_path_name(char *str, int i, t_cmd *cmd)
+char	*export_path_name(char *str, t_cmd *cmd)
 {
-	int	a;
+	int	i;
 
-	a = 0;
+	i = 0;
 	while (str[i] && str[i] == ' ')
 		i++;
 	while (str[i] && (str[i] != ' ' || str[i] != '='))
@@ -31,11 +31,11 @@ char	*export_path_name(char *str, int i, t_cmd *cmd)
 	return (cmd->split[1]);
 }
 
-char	*export_path_var(char *str, int i, t_cmd *cmd)
+char	*export_path_var(char *str, t_cmd *cmd)
 {
-	int	a;
+	int	i;
 
-	a = 0;
+	i = 0;
 	while (str[i] && str[i] == '=')
 		i++;
 	while (str[i] && str[i] != ' ')
@@ -50,6 +50,13 @@ char	*export_path_var(char *str, int i, t_cmd *cmd)
 	return (cmd->split[2]);
 }
 
+char	*split_export_path(char *str, t_cmd *cmd, int index)
+{
+	cmd->split[index - 1] = export_path_name(str, cmd);
+	cmd->split[index] = export_path_var(str, cmd);
+	return (cmd->split[2]);
+}
+
 char	**split_export(char *str, char *dest, t_cmd *cmd)
 {
 	int		i;
@@ -58,20 +65,17 @@ char	**split_export(char *str, char *dest, t_cmd *cmd)
 
 	i = ft_strlen(dest);
 	index = 0;
-	cmd->split = malloc(sizeof(char *) * (4));
-	cmd->split[0] = ft_strdup(dest);
 	tmp = ft_split(str, ' ');
 	while (tmp[index])
-	{
-		if (index == 0)
 			index++;
-		else
-		{
-			
-		}
+	cmd->split = malloc(sizeof(char *) * (index * 2));
+	cmd->split[0] = ft_strdup(dest);
+	index = 1;
+	while (tmp[index])
+	{
+		index++;
+		split_export_path(tmp[index], cmd, index * 2);
 	}
-	cmd->split[1] = export_path_name(str, i, cmd);
-	cmd->split[2] = export_path_var(str, i, cmd);
-	cmd->split[3] = NULL;
+	cmd->split[index * 2] = NULL;
 	return (cmd->split);
 }
