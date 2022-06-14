9643d6bc96e7cb00bb5d67ab5d490e02c471edbd
Pierre <pierre.lvrgn@gmail.com>
2022-06-14 11:52:42 +0200

    parsing export

--- main_work/srcs/parsing/parsing_export.c
+++ main_work/srcs/parsing/parsing_export.c
@@ -6,7 +6,7 @@
 /*   By: plavergn <plavergn@student.42lyon.fr >     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 10:22:56 by plavergn          #+#    #+#             */
-/*   Updated: 2022/05/19 10:23:19 by plavergn         ###   ########.fr       */
+/*   Updated: 2022/06/14 11:49:52 by plavergn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,10 +53,23 @@ char	*export_path_var(char *str, int i, t_cmd *cmd)
 char	**split_export(char *str, char *dest, t_cmd *cmd)
 {
 	int		i;
+	int		index;
+	char	**tmp;
 
 	i = ft_strlen(dest);
+	index = 0;
 	cmd->split = malloc(sizeof(char *) * (4));
 	cmd->split[0] = ft_strdup(dest);
+	tmp = ft_split(str, ' ');
+	while (tmp[index])
+	{
+		if (index == 0)
+			index++;
+		else
+		{
+			
+		}
+	}
 	cmd->split[1] = export_path_name(str, i, cmd);
 	cmd->split[2] = export_path_var(str, i, cmd);
 	cmd->split[3] = NULL;
